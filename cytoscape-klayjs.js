4ea7924c3391bf8970d69eadfec17220a597234c
d2fong <d2fong@uwaterloo.ca>
2017-05-12 19:42:05 -0400

    debugging klayjs extension for compound graph compatibility wip

--- cytoscape-klayjs.js
+++ cytoscape-klayjs.js
@@ -5,7 +5,62 @@
 
     if( !cytoscape ){ return; } // can't register if cytoscape unspecified
 
-    var defaults = {
+    // default values that klay uses
+    var klayDefaults = {
+      'addUnnecessaryBendpoints': 'de.cau.cs.kieler.klay.layered.unnecessaryBendpoints',
+      'alignment': 'de.cau.cs.kieler.alignment',
+      'aspectRatio': 'de.cau.cs.kieler.aspectRatio',
+      'borderSpacing': 'borderSpacing',
+      'compactComponents': 'de.cau.cs.kieler.klay.layered.components.compact',
+      'compactionStrategy': 'de.cau.cs.kieler.klay.layered.nodeplace.compactionStrategy',
+      'contentAlignment': 'de.cau.cs.kieler.klay.layered.contentAlignment',
+      'crossingMinimization': 'de.cau.cs.kieler.klay.layered.crossMin',
+      'cycleBreaking': 'de.cau.cs.kieler.klay.layered.cycleBreaking',
+      'debugMode': 'de.cau.cs.kieler.debugMode',
+      'direction': 'de.cau.cs.kieler.direction',
+      'edgeLabelSideSelection': 'de.cau.cs.kieler.klay.layered.edgeLabelSideSelection',
+      // <broken> 'de.cau.cs.kieler.klay.layered.edgeNodeSpacingFactor': options.edgeNodeSpacingFactor,
+      'edgeRouting': 'de.cau.cs.kieler.edgeRouting',
+      'edgeSpacingFactor': 'de.cau.cs.kieler.klay.layered.edgeSpacingFactor',
+      'feedbackEdges': 'de.cau.cs.kieler.klay.layered.feedBackEdges',
+      'fixedAlignment': 'de.cau.cs.kieler.klay.layered.fixedAlignment',
+      'greedySwitchCrossingMinimization': 'de.cau.cs.kieler.klay.layered.greedySwitch',
+      'hierarchyHandling': 'de.cau.cs.kieler.hierarchyHandling',
+      'inLayerSpacingFactor': 'de.cau.cs.kieler.klay.layered.inLayerSpacingFactor',
+      'interactiveReferencePoint': 'de.cau.cs.kieler.klay.layered.interactiveReferencePoint',
+      'layerConstraint': 'de.cau.cs.kieler.klay.layered.layerConstraint',
+      'layoutHierarchy': 'de.cau.cs.kieler.layoutHierarchy',
+      'linearSegmentsDeflectionDampening': 'de.cau.cs.kieler.klay.layered.linearSegmentsDeflectionDampening',
+      'mergeEdges': 'de.cau.cs.kieler.klay.layered.mergeEdges',
+      'mergeHierarchyCrossingEdges': 'de.cau.cs.kieler.klay.layered.mergeHierarchyEdges',
+      'noLayout': 'de.cau.cs.kieler.noLayout',
+      'nodeLabelPlacement': 'de.cau.cs.kieler.nodeLabelPlacement',
+      'nodeLayering': 'de.cau.cs.kieler.klay.layered.nodeLayering',
+      'nodePlacement': 'de.cau.cs.kieler.klay.layered.nodePlace',
+      'portAlignment': 'de.cau.cs.kieler.portAlignment',
+      'portAlignmentEastern': 'de.cau.cs.kieler.portAlignment.east',
+      'portAlignmentNorth': 'de.cau.cs.kieler.portAlignment.north',
+      'portAlignmentSouth': 'de.cau.cs.kieler.portAlignment.south',
+      'portAlignmentWest': 'de.cau.cs.kieler.portAlignment.west',
+      'portConstraints': 'de.cau.cs.kieler.portConstraints',
+      'portLabelPlacement': 'de.cau.cs.kieler.portLabelPlacement',
+      'portOffset': 'de.cau.cs.kieler.offset',
+      'portSide': 'de.cau.cs.kieler.portSide',
+      'portSpacing': 'de.cau.cs.kieler.portSpacing',
+      'postCompaction': 'de.cau.cs.kieler.klay.layered.postCompaction',
+      'priority': 'de.cau.cs.kieler.priority',
+      'randomizationSeed': 'de.cau.cs.kieler.randomSeed',
+      'routeSelfLoopInside': 'de.cau.cs.kieler.selfLoopInside',
+      'separateConnectedComponents': 'de.cau.cs.kieler.separateConnComp',
+      'sizeConstraint': 'de.cau.cs.kieler.sizeConstraint',
+      'sizeOptions': 'de.cau.cs.kieler.sizeOptions',
+      'spacing': 'de.cau.cs.kieler.spacing',
+      'splineSelfLoopPlacement': 'de.cau.cs.kieler.klay.layered.splines.selfLoopPlacement',
+      'thoroughness': 'de.cau.cs.kieler.klay.layered.thoroughness',
+      'wideNodesOnMultipleLayers': 'de.cau.cs.kieler.klay.layered.wideNodesOnMultipleLayers'
+    };
+
+    var cyDefaults = {
       // define the default options for your layout here
       refreshInterval: 16, // in ms
       refreshIterations: 10, // iterations until thread sends an update
@@ -23,7 +78,7 @@
     };
 
     function Layout( options ){
-      this.options = extend( {}, defaults, options );
+      this.options = extend( {}, cyDefaults, options );
     }
 
     Layout.prototype.run = function(){
@@ -37,68 +92,15 @@
       var positions = [];
 
       (function klayinit(){
-        var properties = function (opts) {
-          var defaults = {
-            "addUnnecessaryBendpoints": "de.cau.cs.kieler.klay.layered.unnecessaryBendpoints",
-            "alignment": "de.cau.cs.kieler.alignment",
-            "aspectRatio": "de.cau.cs.kieler.aspectRatio",
-						"borderSpacing": "borderSpacing",
-						"compactComponents": "de.cau.cs.kieler.klay.layered.components.compact",
-						"compactionStrategy": "de.cau.cs.kieler.klay.layered.nodeplace.compactionStrategy",
-						"contentAlignment": "de.cau.cs.kieler.klay.layered.contentAlignment",
-						"crossingMinimization": "de.cau.cs.kieler.klay.layered.crossMin",
-						"cycleBreaking": "de.cau.cs.kieler.klay.layered.cycleBreaking",
-						"debugMode": "de.cau.cs.kieler.debugMode",
-						"direction": "de.cau.cs.kieler.direction",
-						"edgeLabelSideSelection": "de.cau.cs.kieler.klay.layered.edgeLabelSideSelection",
-						// <broken> "de.cau.cs.kieler.klay.layered.edgeNodeSpacingFactor": options.edgeNodeSpacingFactor,
-						"edgeRouting": "de.cau.cs.kieler.edgeRouting",
-						"edgeSpacingFactor": "de.cau.cs.kieler.klay.layered.edgeSpacingFactor",
-						"feedbackEdges": "de.cau.cs.kieler.klay.layered.feedBackEdges",
-						"fixedAlignment": "de.cau.cs.kieler.klay.layered.fixedAlignment",
-						"greedySwitchCrossingMinimization": "de.cau.cs.kieler.klay.layered.greedySwitch",
-						"hierarchyHandling": "de.cau.cs.kieler.hierarchyHandling",
-						"inLayerSpacingFactor": "de.cau.cs.kieler.klay.layered.inLayerSpacingFactor",
-						"interactiveReferencePoint": "de.cau.cs.kieler.klay.layered.interactiveReferencePoint",
-						"layerConstraint": "de.cau.cs.kieler.klay.layered.layerConstraint",
-						"layoutHierarchy": "de.cau.cs.kieler.layoutHierarchy",
-						"linearSegmentsDeflectionDampening": "de.cau.cs.kieler.klay.layered.linearSegmentsDeflectionDampening",
-						"mergeEdges": "de.cau.cs.kieler.klay.layered.mergeEdges",
-						"mergeHierarchyCrossingEdges": "de.cau.cs.kieler.klay.layered.mergeHierarchyEdges",
-						"noLayout": "de.cau.cs.kieler.noLayout",
-						"nodeLabelPlacement": "de.cau.cs.kieler.nodeLabelPlacement",
-						"nodeLayering": "de.cau.cs.kieler.klay.layered.nodeLayering",
-						"nodePlacement": "de.cau.cs.kieler.klay.layered.nodePlace",
-						"portAlignment": "de.cau.cs.kieler.portAlignment",
-						"portAlignmentEastern": "de.cau.cs.kieler.portAlignment.east",
-						"portAlignmentNorth": "de.cau.cs.kieler.portAlignment.north",
-						"portAlignmentSouth": "de.cau.cs.kieler.portAlignment.south",
-						"portAlignmentWest": "de.cau.cs.kieler.portAlignment.west",
-						"portConstraints": "de.cau.cs.kieler.portConstraints",
-						"portLabelPlacement": "de.cau.cs.kieler.portLabelPlacement",
-						"portOffset": "de.cau.cs.kieler.offset",
-						"portSide": "de.cau.cs.kieler.portSide",
-						"portSpacing": "de.cau.cs.kieler.portSpacing",
-						"postCompaction": "de.cau.cs.kieler.klay.layered.postCompaction",
-						"priority": "de.cau.cs.kieler.priority",
-						"randomizationSeed": "de.cau.cs.kieler.randomSeed",
-						"routeSelfLoopInside": "de.cau.cs.kieler.selfLoopInside",
-						"separateConnectedComponents": "de.cau.cs.kieler.separateConnComp",
-						"sizeConstraint": "de.cau.cs.kieler.sizeConstraint",
-						"sizeOptions": "de.cau.cs.kieler.sizeOptions",
-						"spacing": "de.cau.cs.kieler.spacing",
-						"splineSelfLoopPlacement": "de.cau.cs.kieler.klay.layered.splines.selfLoopPlacement",
-						"thoroughness": "de.cau.cs.kieler.klay.layered.thoroughness",
-						"wideNodesOnMultipleLayers": "de.cau.cs.kieler.klay.layered.wideNodesOnMultipleLayers"
-          };
+        var properties = function (opts, klayOpts) {
           var optionsOut = {};
           var i = 0;
           for(i in opts){
             if(opts[i] !== 'undefined'){
               var j = 0;
-              for(j in defaults){
+              for(j in klayOpts){
                 if(j === i){
-                  optionsOut[defaults[j]] = opts[i];
+                  optionsOut[klayOpts[j]] = opts[i];
                 }
               }
             }
@@ -110,22 +112,23 @@
         var klayChildren = function (nodes) {
           var children = [];
           for (var i = 0; i < nodes.length; i++) {
-            if (nodes[i].data().parent === undefined) {
+            var currNode = nodes[i];
+            if (currNode.data().parent === undefined) {
               children.push({
-                "id": nodes[i].data().id,
-                "width": 4,
-                "height": 4,
-                "children": [],
+                id: currNode.data('id'),
+                width: currNode.outerWidth(),
+                height: currNode.outerHeight(),
+                children: [],
               });
             } else {
               var j;
               for(j = 0; j < children.length; j++){
                 if(children[j].id === nodes[i].data().parent){
                   children[j].children.push({
-                    "id": nodes[i].data().id,
-                    "width": 4,
-                    "height": 4,
-                    "children": []
+                    id: currNode.data('id'),
+                    width: currNode.outerWidth(),
+                    height: currNode.outerHeight(),
+                    children: []
                   });
                 }
               }
@@ -139,40 +142,91 @@
           var i;
           for(i = 0; i < edges.length; i++){
             temp[i] = {
-              "id": edges[i].data().id,
-              "source": edges[i].data().source,
-              "target": edges[i].data().target
-            }
+              id: edges[i].data('id'),
+              source: edges[i].source().data('id'),
+              target: edges[i].target().data('id')
+            };
           }
           return temp;
         };
 
         var graph = {
-          "id": "root",
-          properties: properties(this.options),
-          "children": klayChildren(nodes),
-          "edges": klayEdges(edges)
+          id: 'root',
+          properties: properties(options, klayDefaults),
+          children: klayChildren(nodes),
+          edges: klayEdges(edges)
         };
 
+        console.log(JSON.stringify(graph, null, 4));
+
+        graph = {
+          id: 'root',
+          children: [{
+            id: 'n1',
+            labels: [ { text: 'n1' } ],
+            width: 100,
+            height: 100
+          },
+          {
+            id: 'n2',
+            labels: [ { text: 'n2' } ],
+            width: 100,
+            height: 50,
+            ports: [{
+              id: 'n2_p1',
+              width: 10,
+              height: 10
+            }],
+            children: [{
+              id: 'n3',
+              labels: [ { text: 'n3' } ],
+              width: 40,
+              height: 40
+            },{
+              id: 'n4',
+              labels: [ { text: 'n4' } ],
+              width: 40,
+              height: 40}
+            ],
+            edges: [{
+              id: 'e4',
+              source: 'n3',
+              target: 'n1'
+            }]
+          }],
+          edges: [{
+            id: 'e1',
+            labels: [ { text: 'e1' } ],
+            source: 'n1',
+            target: 'n2',
+            // targetPort: 'n2_p1'
+          }]
+        };
+
+
+        var t;
         klay.layout({
           graph: graph,
           options: {
             spacing: 50
           },
-          success: function (layouted) {
-            //console.log(layouted);
+          success: function (g) {
+            console.log(g);
+            t = g;
           },
-          error: function(){
+          error: function(error){
             //console.log(error);
+            console.log(JSON.stringify(error, null, 4));
           }
         });
+
 
         function recordPositions (nodesIn) {
           for(var i=0; i < nodesIn.length; i++){
             positions.push({
-              "id": nodesIn[i].id,
-              "x": nodesIn[i].x,
-              "y": nodesIn[i].y
+              id: nodesIn[i].id,
+              x: nodesIn[i].x,
+              y: nodesIn[i].y
             });
             if(nodesIn[i].children !== undefined){
               recordPositions(nodesIn[i].children);
@@ -180,16 +234,14 @@
           }
         }
 
+        // console.log(graph.children);
+        // console.log(graph);
         recordPositions(graph.children);
 
         function recordEdges(edgesIn){
           for(var i = 0; i < edgesIn.length; i++){
-            //console.log(edgesIn);
             if(edgesIn[i].bendPoints !== undefined){
-              //console.log(edgesIn[i]);
               for(var j = 0; j < edges.length; j++){
-                //console.log(edges[j].data().id);
-                //console.log(edgesIn[i].id);
                 if(edges[j].data().id === edgesIn[i].id){
                   edges[j].rscratch = edgesIn[i].bendPoints;
                 }
@@ -201,10 +253,11 @@
 
       })();
 
-      var getPos = function( i, ele ){
+      var getPos = function( ele, i ){
         var j = 0;
+        // console.log(positions);
         for(j = 0; j < positions.length; j++){
-          if(positions[j].id === ele.data().id){
+          if(positions[j].id === ele.data('id')){
             return {
               x: positions[i].x,
               y: positions[i].y
@@ -355,7 +408,9 @@
   };
 
   if( typeof module !== 'undefined' && module.exports ){ // expose as a commonjs module
-    module.exports = register( cytoscape, klay || require('klayjs'));
+    module.exports = function ( cytoscape, klay ) {
+      register( cytoscape, klay || require('klayjs'));
+    };
   }
 
   if( typeof define !== 'undefined' && define.amd ){ // expose as an amd/requirejs module
